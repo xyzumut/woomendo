1a9f76edf1be60047f17f546045855e802bd2d5d
UmutGedik <xyzumut06@gmail.com>
2023-06-21 12:08:47 +0300

    dil çevirisi eklendi ve çalıoşıyor

--- woomendo.php
+++ woomendo.php
@@ -7,7 +7,7 @@ Author: Umut
 Text Domain: WooMendo
 Domain Path: /lang
 */
-/* msgfmt -o .\WooMendo-tr_TR.mo WooMendo-tr_TR.po */
+
 require (__DIR__).'/creditCard//woomendo_credit_card_class.php';
 require (__DIR__).'/PaymendoRequest_class.php';
 require (__DIR__).'/WooMendo_init_gateway_class.php';
@@ -21,13 +21,8 @@ add_filter( 'woocommerce_payment_gateways', function ( $gateways ){
 
 add_action('wp_ajax_paymendo_payment_control'		, 'make_payment_control_action' );
 add_action('wp_ajax_nopriv_paymendo_payment_control', 'make_payment_control_action' );
-
 function make_payment_control_action(){
-	
     $post = json_decode(file_get_contents('php://input'), true);
-
-
-
 	# Token api tarafındaki token değil process payment içerisinde oluşturup meta options'lara kaydettiğim şahsi tokenim olmalı
 	# Order ID'de api tarafındaki id değil wordpress tarafındaki id
 	if (!isset($post['token'], $post['order_woocommerce_id'], $post['paid'], $post['order_paymendo_id'], $post['order_number'] )) {
@@ -38,18 +33,14 @@ function make_payment_control_action(){
 			'info' => null
 		]);
 	}
-
 	if ($post['token'] === get_post_meta( $post['order_woocommerce_id'], 'woomendo_paymendo_payment_control_token', true )) {
         # token doğru ve ilgili postmeta kaydı bulunuyor demektir
-
         update_post_meta( $post['order_woocommerce_id'], 'woomendo_paymendo_order_bank_number', $post['order_number']);
-        
         if (json_decode($post['paid']) === 1) {
             $order = wc_get_order( (int)$post['order_woocommerce_id'] );  
 			$order->set_status('processing');
             $order->set_transaction_id( $post['order_paymendo_id'] );
 			$order->save();
-
 			wp_send_json( [
 				'status' => true,
 				'message' => 'Order Payment Confirmed',
@@ -76,7 +67,6 @@ function make_payment_control_action(){
 
 add_action('wp_ajax_paymendo_session'		, 'paymendo_session_return' );
 add_action('wp_ajax_nopriv_paymendo_session', 'paymendo_session_return' );
-
 function paymendo_session_return(){
 	function _return($data = null){
 		if ($data === null) {
@@ -92,14 +82,11 @@ function paymendo_session_return(){
 	}
 
 	if (isset($_GET['operation'], $_GET['order_id']) && !empty(get_post_meta( $_GET['order_id'], 'woomendo_paymendo_payment_redirect_url', true))){
-		
 		if ($_GET['operation'] === 'get_id_and_token' && !empty(get_post_meta($_GET['order_id'],'woomendo_paymendo_payment_redirect_url', true)) && !empty(get_post_meta($_GET['order_id'],'woomendo_paymendo_payment_token_in_api', true))) {
 			$token_of_api = get_post_meta($_GET['order_id'],'woomendo_paymendo_payment_token_in_api', true);
 			$redirect_url = get_post_meta($_GET['order_id'],'woomendo_paymendo_payment_redirect_url', true);
 			$target_url_with_token = get_post_meta($_GET['order_id'],'woomendo_paymendo_payment_target_url_with_token', true);
 			$order_id_in_api = get_post_meta($_GET['order_id'],'woomendo_paymendo_payment_order_id_in_api', true);
-
-			
 			$data = [
 				'token_of_api' => $token_of_api,
 				'redirect_url' => $redirect_url,
@@ -112,8 +99,14 @@ function paymendo_session_return(){
 	_return();
 }
 
-add_action( 'plugins_loaded', function () {
+add_action( 'plugins_loaded', function (){
 	$plugin_dir = basename(dirname(__FILE__));
-	// echo $plugin_dir . '/lang';
-	load_plugin_textdomain( 'Paymendo', false, $plugin_dir . '/lang' );
-} );
+	load_plugin_textdomain( 'WooMendo', false, $plugin_dir . '/lang' );
+});
+/* 
+	poedit veya msgfmt -o .\WooMendo-tr_TR.mo WooMendo-tr_TR.po iş görür
+	isimlendirmeler önemli : 
+	load_plugin_textdomain( 'WooMendo', false, $plugin_dir . '/lang' ) için 'WooMendo' yazan yer 
+	eklentinin adı olacak mutlaka, dosya İsimleride küçük harfli olacak şekilde eklentinin adı ile 
+	başlayıp dil kodu ile devam etmeli tıpkı 'WooMendo-tr_TR.mo' gibi
+*/
